6d7bd8649fce099be8fc58174deb9bed7513697d
Nivin <nivinlouis123@gmail.com>
2025-07-04 23:00:06 +0530

    Added theme colour and password format

--- src/app/manifest.js
+++ src/app/manifest.js
@@ -1,23 +1,23 @@
 export default function manifest() {
-    return {
-      name: 'OnDot Next App',
-      short_name: 'OnDot Next',
-      description: 'A Web App to track attendance of Vidya Academy students',
-      start_url: '/',
-      display: 'standalone',
-      background_color: '#ffffff',
-      theme_color: '#000000',
-      icons: [
-        {
-          src: '/OnDotNext.png',
-          sizes: '192x192',
-          type: 'image/png',
-        },
-        {
-          src: '/OnDotNext.png',
-          sizes: '512x512',
-          type: 'image/png',
-        },
-      ],
-    }
-  }
+  return {
+    name: "OnDot Next App",
+    short_name: "OnDot Next",
+    description: "A Web App to track attendance of Vidya Academy students",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#DFD2FF",
+    icons: [
+      {
+        src: "/OnDotNext.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/OnDotNext.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
--- src/app/page.js
+++ src/app/page.js
@@ -265,7 +265,7 @@ export default function InputComponent() {
                     value={password}
                     onChange={handlePasswordChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5"
-                    placeholder="Enter your ERP Password"
+                    placeholder="d/m/yyyy Format"
                     required
                   />
                 </div>
